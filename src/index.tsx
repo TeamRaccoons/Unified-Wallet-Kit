452f64e2865cc6001c9118dd521e73241948c746
Dickson Pang <34560707+dicksonpys@users.noreply.github.com>
2023-08-08 15:04:06 +0800

    expose base deps

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,7 @@
+// Base libs
+export * from '@solana/wallet-adapter-base';
+export * from '@solana/wallet-adapter-react';
+
 // Contexts
 export { CometKitProvider, useCometKit } from './contexts/CometKitProvider';
 
